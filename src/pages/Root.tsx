69206b4f0420188f7d1bf47ed666546e63244c30
choiwonseokgit <cs0328@hanmail.net>
2024-08-14 01:07:32 +0900

    feat: Root 구조 변경 및 util 함수 추가 datesSlice 변경

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -2,16 +2,12 @@ import GlobalStyle from '@styles/global';
 import { mobileSize } from '@styles/mobile';
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
-import NavBar from './nav-bar';
 
 function Root() {
   return (
     <S.Container>
       <GlobalStyle />
-      <NavBar />
-      <main>
-        <Outlet />
-      </main>
+      <Outlet />
     </S.Container>
   );
 }
--- src/store/datesSlice.ts
+++ src/store/datesSlice.ts
@@ -4,10 +4,15 @@ import { View } from 'react-big-calendar';
 import type { RootState } from '@store/index';
 
 type Dates = [string, string, string];
+//type Dates = string[];
 
-interface TPayload {
+interface TInit {
   date: string;
   view: View;
+}
+
+interface TChangeDates {
+  idx: number;
 }
 
 const initialState: Dates = [
@@ -27,13 +32,61 @@ export const datesSlice = createSlice({
         formatISO(addMonths(new Date(), 1)),
       ];
     },
-    prev: (dates) => {
+    prev: (dates, action: PayloadAction<TChangeDates>) => {
+      const { idx } = action.payload;
+      switch (idx) {
+        case 0:
+          return [
+            dates[0],
+            formatISO(addMonths(dates[0], -2)),
+            formatISO(addMonths(dates[0], -1)),
+          ];
+        case 1:
+          return [
+            formatISO(addMonths(dates[1], -1)),
+            dates[1],
+            formatISO(addMonths(dates[1], -2)),
+          ];
+        case 2:
+          return [
+            formatISO(addMonths(dates[2], -2)),
+            formatISO(addMonths(dates[2], -1)),
+            dates[2],
+          ];
+        default:
+          return dates;
+      }
+
+      return [dates[0], dates[1], formatISO(addMonths(dates[0], -1))];
       return [dates[0], dates[0], dates[2]];
     },
-    next: (dates) => {
+    next: (dates, action: PayloadAction<TChangeDates>) => {
+      const { idx } = action.payload;
+      switch (idx) {
+        case 0:
+          return [
+            dates[0],
+            formatISO(addMonths(dates[0], 1)),
+            formatISO(addMonths(dates[0], 2)),
+          ];
+        case 1:
+          return [
+            formatISO(addMonths(dates[1], 2)),
+            dates[1],
+            formatISO(addMonths(dates[1], 1)),
+          ];
+        case 2:
+          return [
+            formatISO(addMonths(dates[2], 1)),
+            formatISO(addMonths(dates[2], 2)),
+            dates[2],
+          ];
+        default:
+          return dates;
+      }
       return [dates[0], dates[2], dates[2]];
     },
-    init: (_, action: PayloadAction<TPayload>) => {
+    init: (_, action: PayloadAction<TInit>) => {
       const { date, view } = action.payload;
       const changeDate = view === 'day' ? addDays : addMonths;
 
