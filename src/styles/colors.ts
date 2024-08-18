ad048db0e53abe67952d33ea2f2075fabf893707
choiwonseokgit <cs0328@hanmail.net>
2024-08-18 14:35:17 +0900

    chore: css color추가 및 이미지 파일 추가

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -4,12 +4,14 @@ const colors = css`
   :root {
     --black: #000000;
     --white: #ffffff;
+    --gray: #9e9e9e;
 
     //green
     --green01: #f4f8f7;
     --green02: #e9f1ef;
     --green03: #dbede8;
     --green04: #5ca08f;
+    --green05: #2a695c;
   }
 `;
 export default colors;
